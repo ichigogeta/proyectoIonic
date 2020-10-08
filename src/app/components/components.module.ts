1a6bb6213c013eaaea5a3840bed12b96d94d417e
Alejandro Gómez <alejandro.gomez711@gmail.com>
2020-10-08 11:17:54 +0200

    Añadido componente notificaciones y modelo

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -9,6 +9,7 @@ import { AppChatFooterComponent } from './app-chat-footer/app-chat-footer.compon
 import { FormsModule } from '@angular/forms';
 import { AppChatHeaderComponent } from './app-chat-header/app-chat-header.component';
 import { AppLoadingComponent } from './app-loading/app-loading.component';
+import { AppNotificationItemComponent } from './app-notification-item/app-notification-item.component';
 
 const COMPONENTS = [
   AppChatItemComponent,
@@ -16,7 +17,8 @@ const COMPONENTS = [
   AppHeaderComponent,
   AppChatFooterComponent,
   AppChatHeaderComponent,
-  AppLoadingComponent
+  AppLoadingComponent,
+  AppNotificationItemComponent
 ];
 
 @NgModule({
---
+++ src/app/models/Notification.ts
@@ -0,0 +1,7 @@
+/* Datos del objeto Notification*/
+
+export interface Notification{
+    id: number,
+    text:string,
+    subtitle:string
+}
